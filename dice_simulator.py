8230c09e9fc3e0c3f58615814bccfee38928df05
Khushi Patil <khushipatil1147@gmail.com>
2026-06-12 20:06:02 +0530

    Added dice simulator project

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,16 +1,16 @@
 # Simulates 100 dice rolls using NumPy
 import numpy as np
 dice_rolls = np.random.randint(1,7,100)
-print(dice_rolls)
+print("Simulated Dice Rolls (n=100) =", dice_rolls)
 times_6_appear = dice_rolls[dice_rolls == 6]
-print(times_6_appear.size)
+print("Frequency of 6 =", times_6_appear.size)
 avg_roll = dice_rolls.mean()
-print(avg_roll)
+print("Mean =", avg_roll)
 highest_roll = dice_rolls.max()
-print(highest_roll)
+print("Max Roll =", highest_roll)
 lowest_roll = dice_rolls.min()
-print(lowest_roll)
+print("Min Roll =", lowest_roll)
 times_1_appear = dice_rolls[dice_rolls == 1]
-print(times_1_appear.size)
+print("Frequency of 1 =", times_1_appear.size)
 greater_than_4 = dice_rolls[dice_rolls > 4]
-print(greater_than_4.size)
+print("Rolls > 4 =", greater_than_4.size)
